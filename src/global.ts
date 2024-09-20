03184fedac7fb6f450b99d7af5c3fab842f4ff1b
yihan_dai <yihan_dai@intsig.net>
2024-09-20 17:51:07 +0800

    修改websocket端口号

--- src/global.ts
+++ src/global.ts
@@ -1,3 +1,3 @@
 export const USER_KEY = "app_user_info";
 export const ENV_SERVER = "http://localhost:3000";
-export const WS_SERVER = "ws://localhost:8080";
+export const WS_SERVER = "ws://localhost:1234";
