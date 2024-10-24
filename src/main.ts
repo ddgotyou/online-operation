0988e545f1cfb848cd747e93bdb9d3f0e27372e0
yihan_dai <yihan_dai@intsig.net>
2024-10-24 10:06:34 +0800

    vant桌面端mouse事件适配

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "vant/lib/index.css";
+import "@vant/touch-emulator";
 
 createApp(App).use(store).use(router).mount("#app");
